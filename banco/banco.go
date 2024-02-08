5592be82f13ce66866e49224841f0cf932659780
RodrigoF32 <rodrigofreitas3223@outlook.com>
2024-02-08 11:59:30 -0300

    Acertando o BD com gorm

--- banco/banco.go
+++ banco/banco.go
@@ -1,24 +1,17 @@
 package banco
 
 import (
-	"database/sql"
-
-	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 // Conectar retorna um ponteiro de sql
-func Conectar() (*sql.DB, error) {
+func Conectar() (*gorm.DB, error) {
 	stringConexao := "golang:golang@/devbook?charset=utf8&parseTime=True&loc=Local"
 
-	db, erro := sql.Open("mysql", stringConexao)
+	db, erro := gorm.Open(mysql.Open(stringConexao), &gorm.Config{})
 	if erro != nil {
 		return nil, erro
 	}
-
-	//verificar o status da requisição de abertura do bd
-	if erro = db.Ping(); erro != nil {
-		return nil, erro
-	}
-
 	return db, nil
 }
