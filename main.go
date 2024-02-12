5f14e9607d6cb2007f40c1ccfdd23e164b45299c
RodrigoF32 <rodrigofreitas3223@outlook.com>
2024-02-12 17:24:53 -0300

    Metodo Get por ID, finalizado

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"crud-gorm-one/servidor"
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/animais", servidor.PutAnimal).Methods(http.MethodPost)
+	router.HandleFunc("/animais/{id}", servidor.BuscaAnimal).Methods((http.MethodGet))
+
+	fmt.Println("Service On-line")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
--- servidor/servidor.go
+++ servidor/servidor.go
@@ -3,24 +3,30 @@ package servidor
 import (
 	"crud-gorm-one/banco"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 // Animais é a base de atributos de novos animais
 type Animais struct {
-	ID       uint32 `gorm:"primaryKey" json:"id"`
-	Nome     string `json:"nome"`
-	Raca     string `json:"raca"`
-	DonoNome string `json:"dononome"`
+	ID   uint32 `gorm:"primaryKey" json:"id"`
+	Nome string `json:"nome"`
+	Raca string `json:"raca"`
+	Dono string `json:"dono"`
 }
 
-func putAnimal(w http.ResponseWriter, r *http.Request) {
-
+// PutAnimal, usada pra gerar um novo animal
+func PutAnimal(w http.ResponseWriter, r *http.Request) {
+	//deu erro ao rodar, vamos tentar mostrar a tabela que sera usada e verificar no BD
 	db, erro := banco.Conectar()
 	if erro != nil {
 		http.Error(w, "Error ro conect database", http.StatusBadRequest)
+		return
 	}
-	defer db.Close()
+	defer banco.Fechar(db)
 
 	var animal Animais
 
@@ -30,4 +36,38 @@ func putAnimal(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	insereAnimal := db.Create(&animal)
+	if insereAnimal.Error != nil {
+		http.Error(w, "Error to execut Put method: ", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	fmt.Printf("Animal inserted into DB!: %d", animal.ID)
+}
+
+// Busca animal
+func BuscaAnimal(w http.ResponseWriter, r *http.Request) {
+	db, erro := banco.Conectar()
+	if erro != nil {
+		http.Error(w, "Error ro conect database", http.StatusBadRequest)
+		return
+	}
+	//extrair o ID do parâmetro da URL
+	paramsEx := mux.Vars(r)
+	id, erro := strconv.ParseUint(paramsEx["id"], 10, 32)
+	if erro != nil {
+		http.Error(w, "invalid Animal ID", http.StatusBadRequest)
+		return
+	}
+	//consultar bd para obter informações do animal
+	var animal Animais
+	result := db.First(&animal, id)
+	if result.Error != nil {
+		http.Error(w, "Animal not found", http.StatusNotFound)
+		return
+	}
+	//escreve o detalhes do animal na resposta http
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(animal)
 }
