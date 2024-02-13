c93f2035a24a948f7e14b61b3e579f1e0161a999
RodrigoF32 <rodrigofreitas3223@outlook.com>
2024-02-13 14:46:32 -0300

    mudando senha, usuario e nome bd

--- banco/banco.go
+++ banco/banco.go
@@ -7,7 +7,7 @@ import (
 
 // Conectar retorna um ponteiro de sql
 func Conectar() (*gorm.DB, error) {
-	stringConexao := "golang:golang@/devbook?charset=utf8&parseTime=True&loc=Local"
+	stringConexao := "{usuario}:{senha}@/{seu-bd}?charset=utf8&parseTime=True&loc=Local"
 
 	db, erro := gorm.Open(mysql.Open(stringConexao), &gorm.Config{})
 	if erro != nil {
