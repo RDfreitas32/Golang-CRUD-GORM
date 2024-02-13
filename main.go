907b03b7a8b3f47734f35f163769002156008d5e
RodrigoF32 <rodrigofreitas3223@outlook.com>
2024-02-13 13:12:55 -0300

    Criando metodo Put

--- main.go
+++ main.go
@@ -12,9 +12,9 @@ import (
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/animais", servidor.PutAnimal).Methods(http.MethodPost)
-	router.HandleFunc("/animais/{id}", servidor.BuscaAnimal).Methods((http.MethodGet))
-
+	router.HandleFunc("/animais", servidor.InsereAnimal).Methods(http.MethodPost)
+	router.HandleFunc("/animais/{id}", servidor.BuscaAnimal).Methods(http.MethodGet)
+	router.HandleFunc("/animais/{id}", servidor.AtualizaAnimal).Methods(http.MethodPut)
 	fmt.Println("Service On-line")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
--- servidor/servidor.go
+++ servidor/servidor.go
@@ -19,7 +19,7 @@ type Animais struct {
 }
 
 // PutAnimal, usada pra gerar um novo animal
-func PutAnimal(w http.ResponseWriter, r *http.Request) {
+func InsereAnimal(w http.ResponseWriter, r *http.Request) {
 	//deu erro ao rodar, vamos tentar mostrar a tabela que sera usada e verificar no BD
 	db, erro := banco.Conectar()
 	if erro != nil {
@@ -70,4 +70,49 @@ func BuscaAnimal(w http.ResponseWriter, r *http.Request) {
 	//escreve o detalhes do animal na resposta http
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(animal)
+}
+
+func AtualizaAnimal(w http.ResponseWriter, r *http.Request) {
+	db, erro := banco.Conectar()
+	if erro != nil {
+		http.Error(w, "Erro ao abrir banco de dados: ", http.StatusInternalServerError)
+	}
+
+	// Extrair o ID do parâmetro da URL
+	paramEx := mux.Vars(r)
+	id, erro := strconv.ParseUint(paramEx["id"], 10, 32)
+	if erro != nil {
+		http.Error(w, "ID Inválido: ", http.StatusBadRequest)
+		return
+	}
+	// Decodificar o JSON da solicitação para obter os novos dados do animal
+	var animalAtualizado Animais
+
+	erro = json.NewDecoder(r.Body).Decode(&animalAtualizado)
+	if erro != nil {
+		http.Error(w, "Erro ao ler o corpo da requisição: ", http.StatusBadRequest)
+		return
+	}
+
+	// Consultar o banco de dados para obter o animal existente
+	var animalCadastrado Animais
+	resultado := db.First(&animalCadastrado, id)
+	if resultado.Error != nil {
+		http.Error(w, "Animal não encontrado", http.StatusNotFound)
+		return
+	}
+	// Atualizar as informações do animal existente com os novos dados
+	animalCadastrado.Nome = animalAtualizado.Nome
+	animalCadastrado.Raca = animalAtualizado.Raca
+	animalCadastrado.Dono = animalAtualizado.Dono
+
+	//Salvar as alterações no banco de dados:
+	resultado = db.Save(&animalCadastrado)
+	if resultado.Error != nil {
+		http.Error(w, "Erro ao atualizar: ", http.StatusInternalServerError)
+		return
+	}
+	// Escrever uma resposta de sucesso
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Animal Atualizado com Sucesso"))
 }
